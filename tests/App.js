3da4d77718b9dcc6ad28688b9123740cacc2ba28
Michał Lach <kontakt@michal-lach.pl>
2016-05-17 18:35:43 +0200

    unit tests solution

---
+++ tests/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { expect } from 'chai';
+import { shallow, mount, render } from 'enzyme';
+import App from '../src/js/components/App';
+
+describe("A suite", function() {
+  it("contains spec with an expectation", function() {
+    expect(shallow(<App />).contains(<h1>Hello</h1>)).to.equal(true);
+  });
+});
